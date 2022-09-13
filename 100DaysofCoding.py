a7e7e4b4cd827d907dc99af65703d2d06ac5e60f
Sanusi David <sanusidavid360@gmail.com>
2022-09-13 21:39:05 +0100

    question 2 solved

--- 100DaysofCoding.py
+++ 100DaysofCoding.py
@@ -6,3 +6,23 @@ inp2 = "Hello"
 inp3 = "Amazing"
 
 print(len(inp3))
+
+#2nd Question
+
+#index of a string.
+y = "Pizza"  
+
+num = int(input("Enter index: "))
+print(y[num])
+
+
+s = ""
+if len(s) == 0:
+    print("Empty String")
+
+z = "World"
+num2 = int(input("new number sir!: "))
+for i in z:
+    if num2 > len(i):
+        print("i is out of range")
+        break
