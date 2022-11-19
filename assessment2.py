3c31e59a5f213c004e3033b75efd2c8ead100aa4
sirnusi <davidsanusi360@gmail.com>
2022-11-19 15:25:38 +0100

    #challenge

--- assessment2.py
+++ assessment2.py
@@ -1,15 +1,14 @@
-# def caeser_cipher(string, offset):
-        
-string1 = input('Enter string: ')
-offset = int(input('Number: '))
-plain = 'abcdefghijklmnopqrstuvwxyz'
-cipher = 'xyzabcdefghijklmnopqrstuvw'
+def caeser_cipher(string, offset):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-new_list = []
+    encoded_string = ''
 
-for i in string1:
-    if i in plain:
-        addition = plain.index(i)
-        new_list.append(cipher[addition + offset])
+    for character in string:
+        position = alphabet.index(character)
+        offset_position = position - offset
+        # No need to handle negative positions because of negative indexing
+        encoded_character = alphabet[offset_position]
+        encoded_string += encoded_character
 
-print(new_list)
+    return encoded_string
